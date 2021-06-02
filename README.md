6e72c540a78e314e83a538b0f3ca7e956a13beec
Mariana Andrade <andrade.marianaferreira@gmail.com>
2021-06-01 23:52:17 -0300

    Incluindo arquivos JS, Json e o Readme

---
+++ README.md
@@ -0,0 +1,26 @@
+## Lógica aplicada - Arrays, Objetos e Métodos :information_desk_person: :computer: 
+
+**Sintaxe básica JS**
+
+
+- Uso de Objetos
+- Uso de Arrays
+- Métodos de Iteração
+    
+
+No exercídcio dessa semana eu criei o código a partir de uma Array de Objetos.
+Usei os métodos de Iteração Filter para retornar um Array com o sobjetos que 
+foram filtrados e o Sort para ordenar os livros pela quantidade de páginas. 
+
+Usei o Require para importar os modulos dentro da aplicação e aprendi o uso
+do toLocaleUpperCase para converter todas as letras da entrada para maiúsculas.
+
+Usei o console.table para imprimir em tabelas os objetos do Array e utilizei 
+Arrow Function para começar a usar o novo modo de declarar a função.
+
+Descobri uma nova forma de escrever um operador condicional (condicao ? valor1 : valor2).
+
+E quebrei muito a cabeça com os erros do npm e os pacotes do Node.js.
+
+
+
---
+++ app.js
@@ -0,0 +1,61 @@
+const livros = require('./database');
+
+livros.sort(function (a, b) {
+	
+	return (a.pagina > b.pagina) ? 1 : ((b.pagina > a.pagina) ? -1 : 0);
+ 
+});
+
+
+const pegarEntrada = require('readline-sync');
+
+
+const descobrirLivros = pegarEntrada.question('Deseja encontrar livros pela categoria?  (S/N)');
+
+if (descobrirLivros.toLocaleUpperCase() === 'S'){
+    console.log('Esses são as categorias listadas:');
+    console.log('Romance', 'Aventura', 'Ciencias Sociais', 'Historia', 'Infantil', 'Computacao', 'Ficcao', 'Poesia');
+
+    const qualCategoria = pegarEntrada.question('Qual categoria voce escolhe?');
+
+    const categoria = livros.filter(livros => livros.categoria === qualCategoria);
+
+    console.table(categoria);
+    
+} else{
+    console.log('Essas sao todas as categorias de livros:  ');
+    console.table(livros);
+}
+
+
+
+const recomendacao = pegarEntrada.question('Deseja recomendação de livros para ler?  (S/N)');
+
+if (recomendacao.toLocaleUpperCase() === 'S'){
+    console.log('Essas são as recomendações: ');
+
+    const recomenda = livros.filter(livros => livros.recomenda === true);
+
+    console.table(recomenda);
+    
+} else{
+    
+}
+
+
+
+const desejo = pegarEntrada.question('Deseja ver a lista de desejos?  (S/N)');
+
+if (desejo.toLocaleUpperCase() === 'S'){
+    console.log('Essa é a lista de desejos: ');
+
+    const deseja = livros.filter(livros => livros.leu === false);
+
+    console.table(deseja);
+    
+} else{
+    
+}
+
+
+
